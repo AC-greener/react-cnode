758a5697cf310c10afa765730d1363b95efffd78
tong tong <1053673440@qq.com>
2019-01-24 18:53:01 +0800

    结合redux可以关闭登陆框

--- src/components/aside/index.jsx
+++ src/components/aside/index.jsx
@@ -2,33 +2,99 @@ import React from 'react'
 import { connect } from 'react-redux'
 import './style.css'
 class Aside extends React.Component {
+  
   constructor(props) {
     super(props)
+    console.log(
+    )
   }
   render() {
-    return (
-      <aside className='clearfix'>
-        <div className="left">
-          {this.props.n}
-          <button id="add1" onClick={()=> this.props.add1()}>+1</button>
-          <div className="loginWrapper">
-            <img src="../../statics/imgs/ball.ico" alt="" />
+    if(this.props.modalStatus) {
+      return (
+        <aside className='clearfix'>
+          <div className="left">
+            <div className="loginWrapper">
+              <svg  className="icon iconDribbble" aria-hidden="true">
+                <use xlinkHref="#icon-dribbble"></use>
+              </svg>
+              <div>
+                Login
+              </div>
+              <div>
+                Tap here to lohin
+              </div>
+              <img src="../../statics/imgs/ball.ico" alt="" />
+            </div>
+            <div className='itemWrapper'>
+              <div className='item'>
+                <span>
+                  <svg className="icon " aria-hidden="true">
+                    <use xlinkHref="#icon-home"></use>
+                  </svg>
+                </span>
+                <span>home</span>
+              </div>
+              <div className='item'>
+                <span>
+                  <svg className="icon " aria-hidden="true">
+                    <use xlinkHref="#icon-explore"></use>
+                  </svg>
+                </span>
+                <span>
+                  Explore
+                </span>
+              </div>
+            </div>
+            <div className='itemWrapper'>
+              <div className='item'>
+              <span>
+                  <svg className="icon " aria-hidden="true">
+                    <use xlinkHref="#icon-settings"></use>
+                  </svg>
+                </span>
+                <span>
+                  Settings
+                </span>
+              </div>
+              <div className='item'>
+              <span>
+                  <svg className="icon " aria-hidden="true">
+                    <use xlinkHref="#icon-about"></use>
+                  </svg>
+                </span>
+                <span>
+                {/* <button id="add1" onClick={()=> this.props.add1()}>+1</button>
+                  {this.props.n} */}
+                  Report a bug
+                </span>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="right">right</div>
-      </aside>
-    )
+          <div className="right" onClick={() => this.props.closeLoginModal()}>right</div>
+        </aside>
+      )
+    } else {
+      return null 
+    }
+
   }
 }
 const mapStateToProps = (state) => {
   return {
-    n: state.n
+    n: state.n,
+    modalStatus: state.showLoginModal
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    add1: () => {dispatch({type: 'add', payload: 1})},
+    add1: () => {
+      dispatch({type: 'add', payload: 1})
+    },
+    closeLoginModal() {
+      console.log('colse')
+      dispatch({type: 'change_login_modal', value: false})
+    }
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Aside)
--- src/index.js
+++ src/index.js
@@ -6,17 +6,23 @@ import { Provider } from 'react-redux'
 
 const reducer = (state, action) => {
   if(state === undefined) {
-    return {n: 10}
+    return {n: 10, showLoginModal: true}
   } else {
     if(action.type === 'add') {
       let newState = {n: state.n + action.payload}
+      return newState
+    } else if(action.type === 'change_login_modal') {
+      console.log('value', action.value)
+      let newState = {n: state.n, showLoginModal: action.value}
       return newState
     } else {
       return state
     }
   }
 }
+
 const store = createStore(reducer)
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
