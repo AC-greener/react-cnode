9a4f0f330cd4af33b7d93e07f325b5d4fc15c130
tongtong <1053673440@qq.com>
2019-02-25 12:37:35 +0800

    '初始化shots页面并添加了一些css'

--- src/pages/shots/index.jsx
+++ src/pages/shots/index.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import './style.css'
 
 class Shots extends React.Component {
   render() {
     return (
-      <div>
-        这是shots路由
+      <div className='shotsWrap'>
+        <div className="avatarWrap">
+          <div className="avatar">
+            <img src={require('../../statics/imgs/avatar1.jpg')} alt=""/>
+          </div>
+          <div className="intro">
+            <div className="name">zhangsan</div>
+            <div className="from">by Divan Raj in Fitness scheduling app</div>
+          </div>
+        </div>
+        <div className="savelike">
+          <button> Save </button>
+          <button> Like </button>
+        </div>
+        <div className="img">
+          img
+        </div>
       </div>
     )
   }
