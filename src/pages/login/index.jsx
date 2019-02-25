3d196d69820128cff65cdd55c5821b24140f2cef
tongtong <1053673440@qq.com>
2019-02-25 09:12:20 +0800

    '修改登录页面css'

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
 class Login extends React.Component {
-  constructor(props) {
-    super(props)
+  constructor (props) {
+    super (props);
   }
-  render() {
+  render () {
     return (
-      <div className='loginWrapper'>
-          登录页面
+      <div className="loginWrapper">
+        <header>
+          <div className="name">Dribble</div>
+          <div className="signin">Sign in</div>
+        </header>
+        <div className="box">
+          <div className="item">
+            <a>Sign in with Twitter</a>
+          </div>
+          <div className="item">
+            <a>Sign in with Facebook</a>
+          </div>
+          <div className="item">
+            <a>Sign in with Google</a>
+          </div>
+        </div>
+        <div className="validation">
+          <div className="username item">
+          <div>
+              <label htmlFor="username">Username or Email</label>
+          </div>
+            <input type="text" id="username" />
+          </div>
+          <div className="passwordlable item">
+          </div>
+          <div className="password item">
+          <div>
+              <label htmlFor="username">Password</label>
+          </div>
+            <input type="text" id='password'/>
+          </div>
+          <div className='sign'>
+            <button>sign in</button>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
-export default Login
+export default Login;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -2,26 +2,23 @@ import React from 'react'
 import './style.css'
 import axios from 'axios'
 class Main extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <div className='contentWrapper'>
         <div className='item'>
-          <img src={require('../../statics/imgs/item1.jpg')} />
+          <img src={require('../../statics/imgs/item1.jpg')} alt='' />
         </div>
         <div className='item'>
-          <img src={require('../../statics/imgs/item2.png')} alt=""/>
+          <img src={require('../../statics/imgs/item2.png')} alt="img"/>
         </div>
         <div className='item'>
-          <img src={require('../../statics/imgs/item3.png')} alt="" />
+          <img src={require('../../statics/imgs/item3.png')} alt="img" />
         </div>
         <div className='item'>
-          <img src={require('../../statics/imgs/item4.png')} alt="" />
+          <img src={require('../../statics/imgs/item4.png')} alt="img" />
         </div>
         <div className='item'>
-          <img src={require('../../statics/imgs/item5.png')} alt="" />
+          <img src={require('../../statics/imgs/item5.png')} alt="img" />
         </div>
         <div className='item'>
           <img src={require('../../statics/imgs/item6.png')} alt="" />
