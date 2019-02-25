9ac6792f7529729f3cff90bcc6eb04c685875163
tongtong <1053673440@qq.com>
2019-02-25 09:48:44 +0800

    '完成登录页面样式并添加icon'

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,36 +8,54 @@ class Login extends React.Component {
     return (
       <div className="loginWrapper">
         <header>
-          <div className="name">Dribble</div>
-          <div className="signin">Sign in</div>
+          <div className="name">
+          <img src={require('../../statics/imgs/dribbble.png')} alt=""/>
+          </div>
+          <div className="signin">
+            <h2>Sign in</h2>
+          </div>
         </header>
         <div className="box">
           <div className="item">
-            <a>Sign in with Twitter</a>
+            <a>
+              <svg className="icon" aria-hidden="true">
+                <use xlinkHref="#icon-twitter" />
+              </svg>
+              Sign in with Twitter</a>
           </div>
           <div className="item">
-            <a>Sign in with Facebook</a>
+            <a>
+              <svg  className="icon" aria-hidden="true">
+                <use xlinkHref="#icon-facebook" />
+              </svg>
+              Sign in with Facebook
+              </a>
           </div>
           <div className="item">
-            <a>Sign in with Google</a>
+          
+            <a>
+              <svg className="icon" aria-hidden="true">
+                <use xlinkHref="#icon-google" />
+              </svg>
+              Sign in with Google
+              
+              </a>
           </div>
         </div>
         <div className="validation">
-          <div className="username item">
-          <div>
+          <div className="username">
+            <div className='label'>
               <label htmlFor="username">Username or Email</label>
-          </div>
+            </div>
             <input type="text" id="username" />
           </div>
-          <div className="passwordlable item">
-          </div>
-          <div className="password item">
-          <div>
+          <div className="password">
+            <div className='label'>
               <label htmlFor="username">Password</label>
+            </div>
+            <input type="text" id="password" />
           </div>
-            <input type="text" id='password'/>
-          </div>
-          <div className='sign'>
+          <div className="sign">
             <button>sign in</button>
           </div>
         </div>
