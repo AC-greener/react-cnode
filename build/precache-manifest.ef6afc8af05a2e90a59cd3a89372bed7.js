c2126103cbdc56dec57ef5b084ef3eff2e6206ba
tongtong <1053673440@qq.com>
2019-03-01 09:10:16 +0800

    '重新build并添加homepage'

---
+++ build/precache-manifest.ef6afc8af05a2e90a59cd3a89372bed7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f67e0a5a10b1b5b507f2ab733c9985a6",
+    "url": "/static/media/dribbble.f67e0a5a.png"
+  },
+  {
+    "revision": "8650aeafb73f2fa3698cb1992071a2e7",
+    "url": "/static/media/bg.8650aeaf.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "501e5735b8b4b9ad51c2",
+    "url": "/static/js/main.501e5735.chunk.js"
+  },
+  {
+    "revision": "cefb6507febac9dfa079",
+    "url": "/static/js/1.cefb6507.chunk.js"
+  },
+  {
+    "revision": "501e5735b8b4b9ad51c2",
+    "url": "/static/css/main.290b406b.chunk.css"
+  },
+  {
+    "revision": "bd097af6886ad8de6e451784cf2748fa",
+    "url": "/index.html"
+  }
+];
