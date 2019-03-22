361da183f38762a857f088d4e613003b43ac5640
tongtong <1053673440@qq.com>
2019-03-22 23:34:45 +0800

    '引入sass'

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Aside from '../src/components/aside/index'
 import Main from './pages/main/index'
 import Login from './pages/login/index'
 import Shots from './pages/shots/index'
-import './style.css'
+import './style.scss'
 
 import { BrowserRouter as Router, Route } from "react-router-dom"
 
@@ -11,10 +11,6 @@ class App extends Component {
   render() {
     return (
       <Router >
-        {/* 
-   "homepage" : "https://ac-greener.github.io/react-dribbble/build",
-        
-        */}
         <div className='app'>
           <Route exact path='/' component={Main} />
           <Route exact path='/aside' component={Aside}/>
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css';
+import './style.scss';
 import {Link} from 'react-router-dom';
 
 class Login extends React.Component {
